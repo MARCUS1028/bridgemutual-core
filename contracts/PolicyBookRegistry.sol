1a4c4e207cc0d34690621740c4d6c96d6664d084
Yaroslav Khodakovskij <yaroslav.khodakovsky@x.applicature.com>
2021-01-11 16:27:40 +0200

    Extend implementation of registry
    Change interface, implementation of policy book registry and added tests

--- contracts/PolicyBookRegistry.sol
+++ contracts/PolicyBookRegistry.sol
@@ -1,33 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.7.4;
 
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./interfaces/IPolicyBook.sol";
 import "./interfaces/IPolicyBookRegistry.sol";
 
-contract PolicyBookRegistry {
-  address policyFabricAddress;
-  uint256 policyBooksCount;
+contract PolicyBookRegistry is IPolicyBookRegistry, Ownable {
+  using Math for uint256;
 
-  constructor(address _policyFabricAddress) {
-    policyFabricAddress = _policyFabricAddress;
-  }
+  address public policyFabricAddress;
+  uint256 private policyBooksCount;
 
-  mapping(address => address) policies;
+  address[] private policies;
+  mapping(address => address) private policiesByAddress;
+
+  event Added(address insured, address at);
 
   modifier onlyPolicyFabric() {
     require(msg.sender == policyFabricAddress, "Caller is not a policyFabric contract");
     _;
   }
 
-  function add(address _insuredContract, address _policyBook) external onlyPolicyFabric() {
-    require(policies[_insuredContract] == address(0), "PolicyBook for the contract is already created");
+  function setPolicyFabricAddress(address _policyFabricAddress) external onlyOwner {
+    policyFabricAddress = _policyFabricAddress;
+  }
 
-    policies[_insuredContract] = _policyBook;
+  function add(address _insuredContract, address _policyBook) external override onlyPolicyFabric() {
+    require(policiesByAddress[_insuredContract] == address(0), "PolicyBook for the contract is already created");
+
+    policies.push(_policyBook);
+    policiesByAddress[_insuredContract] = _policyBook;
     policyBooksCount++;
+
+    emit Added(_insuredContract, _policyBook);
+  }
+
+  function count() external view override returns (uint256 _policyBooksCount) {
+    return policyBooksCount;
+  }
+
+  function list(uint256 _offset, uint256 _limit)
+    external
+    view
+    override
+    returns (uint256 _policyBooksCount, address[] memory _policyBooks)
+  {
+    uint256 to = (_offset + _limit).min(policyBooksCount).max(_offset);
+    uint256 size = to - _offset;
+    address[] memory result = new address[](size);
+    for (uint256 i = _offset; i < to; i++) {
+      result[i - _offset] = policies[i];
+    }
+
+    return (size, result);
+  }
+
+  function policyBookFor(address _contract) external view override returns (address _policyBook) {
+    return policiesByAddress[_contract];
   }
 
   function stats(address[] calldata _policyBooks)
     external
+    override
     returns (
       uint256[] memory _yearlyCost,
       uint256[] memory _maxCapacities,
@@ -46,6 +82,7 @@ contract PolicyBookRegistry {
 
   function statsByInsuredContracts(address[] calldata _insuredContracts)
     external
+    override
     returns (
       uint256[] memory _yearlyCost,
       uint256[] memory _maxCapacities,
