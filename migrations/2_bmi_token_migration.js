3d529c7dec27c777272b4b9338bbdb15cc5eab1a
Yaroslav Khodakovskij <yaroslav.khodakovsky@x.applicature.com>
2021-01-27 16:29:22 +0200

    Remote ownership trasfer from migrations

--- migrations/2_bmi_token_migration.js
+++ migrations/2_bmi_token_migration.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 
 const TokenContract = artifacts.require('BMIToken');
@@ -8,8 +7,4 @@ const ownerAddress = '0x0000000000000000000000000000000000000003';
 
 module.exports = async (deployer) => {
   await deployProxy(TokenContract, [ownerAddress], {deployer});
-  const token = await TokenContract.deployed();
-
-  await token.transferOwnership(ownerAddress);
-  assert((await token.owner()) == ownerAddress);
 };
--- migrations/3_bmi_vesting_migration.js
+++ migrations/3_bmi_vesting_migration.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 
 const TokenContract = artifacts.require('BMIToken');
@@ -6,14 +5,10 @@ const VestingContract = artifacts.require('BMITokenVesting');
 
 // TODO fill with final data
 const tokenGenerationTimestamp = 1609325103;
-const ownerAddress = '0x0000000000000000000000000000000000000003';
 
 module.exports = async (deployer) => {
   await deployProxy(VestingContract, [tokenGenerationTimestamp], {deployer});
   const vesting = await VestingContract.deployed();
   const token = await TokenContract.deployed();
   await vesting.setToken(token.address);
-
-  await vesting.transferOwnership(ownerAddress);
-  assert((await vesting.owner()) == ownerAddress);
 };
