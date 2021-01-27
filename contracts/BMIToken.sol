bc616f8c3bc4d67f0b657793cefc87089b3e8dda
Yaroslav Khodakovskij <yaroslav.khodakovsky@x.applicature.com>
2021-01-27 19:08:39 +0200

    Make BMI token upgradable permit

--- contracts/BMIToken.sol
+++ contracts/BMIToken.sol
@@ -2,15 +2,16 @@
 pragma solidity =0.7.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./erc20permit-upgradable/ERC20PermitUpgradeable.sol";
 
 
-contract BMIToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+contract BMIToken is Initializable, ERC20PermitUpgradeable, OwnableUpgradeable {
   uint256 constant TOTAL_SUPPLY = 160 * (10**6) * (10**18);
 
   function initialize(address tokenReceiver) public initializer {
     __ERC20_init("BridgeMutual Insurance", "BMI");
+    __ERC20Permit_init("BridgeMutual Insurance");
     __Ownable_init();
     _mint(tokenReceiver, TOTAL_SUPPLY);
   }
