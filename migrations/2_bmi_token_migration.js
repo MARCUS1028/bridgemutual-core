c544dc98bf4d0c5a17a9dcdfcdd9da9469e9b085
Yaroslav Khodakovskij <yaroslav.khodakovsky@x.applicature.com>
2021-01-27 17:03:15 +0200

    Remove initialization from migration of token

--- migrations/2_bmi_token_migration.js
+++ migrations/2_bmi_token_migration.js
@@ -2,9 +2,6 @@ const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 
 const TokenContract = artifacts.require('BMIToken');
 
-// TODO fill with final data
-const ownerAddress = '0x0000000000000000000000000000000000000003';
-
 module.exports = async (deployer) => {
-  await deployProxy(TokenContract, [ownerAddress], {deployer});
+  await deployProxy(TokenContract, [], {deployer, initializer: false});
 };
