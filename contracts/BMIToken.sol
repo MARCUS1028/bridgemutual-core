f6cba30bf0014361276bc1900e67fed083f784d4
Yaroslav Khodakovskij <yaroslav.khodakovsky@x.applicature.com>
2021-01-27 16:12:34 +0200

    Change token to ownable and migration to transfer ownership

--- contracts/BMIToken.sol
+++ contracts/BMIToken.sol
@@ -3,12 +3,15 @@ pragma solidity =0.7.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BMIToken is Initializable, ERC20Upgradeable {
+
+contract BMIToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
   uint256 constant TOTAL_SUPPLY = 160 * (10**6) * (10**18);
 
-  function initialize(address vesting_address) public initializer {
+  function initialize(address tokenReceiver) public initializer {
     __ERC20_init("BridgeMutual Insurance", "BMI");
-    _mint(vesting_address, TOTAL_SUPPLY);
+    __Ownable_init();
+    _mint(tokenReceiver, TOTAL_SUPPLY);
   }
 }
--- migrations/2_bmi_token_and_vesting_migration.js
+++ migrations/2_bmi_token_and_vesting_migration.js
@@ -1,15 +1,22 @@
+const assert = require('assert');
 const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 
 const TokenContract = artifacts.require('BMIToken');
 const VestingContract = artifacts.require('BMITokenVesting');
 
-// TODO fill with final timestamp
+// TODO fill with final data
 const tokenGenerationTimestamp = 1609325103;
+const ownerAddress = '0x0000000000000000000000000000000000000003';
 
 module.exports = async (deployer) => {
   await deployProxy(VestingContract, [tokenGenerationTimestamp], {deployer});
   const vesting = await VestingContract.deployed();
-  await deployProxy(TokenContract, [vesting.address], {deployer});
+  await deployProxy(TokenContract, [ownerAddress], {deployer});
   const token = await TokenContract.deployed();
   await vesting.setToken(token.address);
+
+  await vesting.transferOwnership(ownerAddress);
+  await token.transferOwnership(ownerAddress);
+  assert((await vesting.owner()) == ownerAddress);
+  assert((await token.owner()) == ownerAddress);
 };
